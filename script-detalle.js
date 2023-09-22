e12b423ea3a6aebd5bbf2a9459d404b62645237b
Ayarachi Fuentes Hector Daniel <135895299+HectorDanielAyarachiFuentes@users.noreply.github.com>
2023-09-22 16:40:54 -0300

    Update script-detalle.js

--- script-detalle.js
+++ script-detalle.js
@@ -41,3 +41,53 @@ function goToIndex() {
 }
 
 ///// FUNCION COMPRA
+let cerrarVentana = false;
+        
+        function realizarCompra() {
+            const multiplicador = parseFloat(document.getElementById('multiplicador').value);
+            const precioTexto = document.getElementById('precioPokemon').textContent;
+        
+            // Extraer el precio numérico del texto (por ejemplo, "$50.00" -> 50.00)
+            const precio = parseFloat(precioTexto.match(/\d+\.\d+/));
+        
+            if (isNaN(precio) || isNaN(multiplicador) || multiplicador < 1) {
+                alert("Por favor, ingrese valores válidos.");
+                return;
+            }
+        
+            const resultado = precio * multiplicador;
+        
+            // Dar formato al resultado con puntos cada tres dígitos
+            const resultadoFormateado = resultado.toLocaleString();
+        
+            // Mostrar el resultado formateado y solicitar confirmación
+            const confirmacion = window.confirm(`El precio total es: US$ ${resultadoFormateado}. ¿Estás seguro de realizar la compra?`);
+        
+            if (confirmacion) {
+                // Cambiar el texto del botón
+                const botonCompra = document.querySelector('.compra-button');
+                botonCompra.textContent = 'Enviar Producto';
+        
+                // Mostrar la dirección inventada
+                const direccion = '123 Calle Ficticia, Ciudad Imaginaria';
+                window.alert(`Estás comprando un Pokémon. Tu pedido está siendo enviado. ¡Gracias por comprar nuestros Pokémon clonados!`);
+            } else {
+                // Cancelar la compra y marcar para cerrar la ventana de alerta
+                cerrarVentana = true;
+                window.alert('Compra cancelada.');
+            }
+        }
+        
+        // Obtener el botón de compra
+        const botonCompra = document.querySelector('.compra-button');
+        
+        // Agregar un evento de clic al botón
+        botonCompra.addEventListener('click', function() {
+            // Cerrar la ventana de alerta si se marcó para cerrar
+            if (cerrarVentana) {
+                window.close();
+            } else {
+                // Mostrar mensaje de alerta
+                window.alert('Enviando... Solo espera tu Pokémon');
+            }
+        });
