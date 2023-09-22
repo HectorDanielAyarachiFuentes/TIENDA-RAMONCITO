3e33f2e185c56506513403ede8825147f4805d59
Ayarachi Fuentes Hector Daniel <135895299+HectorDanielAyarachiFuentes@users.noreply.github.com>
2023-09-22 16:03:04 -0300

    Update script-detalle.js

--- script-detalle.js
+++ script-detalle.js
@@ -39,55 +39,5 @@ document.addEventListener("DOMContentLoaded", function () {
 function goToIndex() {
     window.location.href = "index-picachu.html";
 }
-function realizarCompra() {
-    const cantidad = parseInt(document.getElementById("quantity").value);
-    const precioUnitario = parseFloat(selectedPokemon.precio);
-    
-    if (isNaN(cantidad) || cantidad < 1) {
-        alert("Por favor, ingrese una cantidad válida.");
-    } else {
-        const total = cantidad * precioUnitario;
 
-        const confirmacion = confirm(`¿Desea comprar ${cantidad} ${selectedPokemon.nombre}(s) por un total de $${total.toFixed(2)}?`);
-        
-        if (confirmacion) {
-            // Aquí puedes agregar la lógica para realizar la compra
-            alert("¡Compra realizada con éxito!");
-        } else {
-            alert("Compra cancelada.");
-        }
-    }
-}
-
-function realizarCompra() {
-    const multiplicador = parseFloat(document.getElementById('multiplicador').value);
-    const precio = parseFloat(document.getElementById('precio').textContent);
-    const resultado = precio * multiplicador;
-
-    // Dar formato al resultado con puntos cada tres dígitos
-    const resultadoFormateado = resultado.toLocaleString();
-
-    // Mostrar el resultado formateado y solicitar confirmación
-    const confirmacion = window.confirm(`El precio total es: US$ ${resultadoFormateado}. ¿Estás seguro de realizar la compra?`);
-
-    if (confirmacion) {
-        // Cambiar el texto del botón
-        const botonCompra = document.querySelector('.compra-button');
-        botonCompra.textContent = 'Enviar Producto';
-
-        // Mostrar la dirección inventada
-        const direccion = '123 Calle Ficticia, Ciudad Imaginaria';
-        window.alert(`Estás comprando un Pokémon. Haz clic en "Enviar Producto" para enviarlo a la siguiente dirección:\n\n${direccion}`);
-    }
-}
-// Obtener el botón de compra
-const botonCompra = document.querySelector('.compra-button');
-
-// Agregar un evento de clic al botón
-botonCompra.addEventListener('click', function() {
-// Mostrar mensaje de alerta
-window.alert('Enviando... Solo espera tu Pokémon');
-
-// Redirigir a la página "index-picachu.html"
-window.location.href = 'index-picachu.html';
-});
+///// FUNCION COMPRA
